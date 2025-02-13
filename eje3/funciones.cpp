e54205014699455eb5bddf9e7bf3a0c73dc2df9f
Andres Rubio <andres.rubio-c@mail.escuelaing.edu.co>
2025-02-13 14:11:16 -0500

    ejercicio funcion3 - Samuel Moreno y Andres Rubio

--- eje3/funciones.cpp
+++ eje3/funciones.cpp
@@ -14,6 +14,10 @@ double funeval( const double &x ){
   return sin(M_PI*5*x/100);
 }
 
+double funeval3( const double &x){
+  return cos(x*M_PI/25);
+}
+
 void cruces_cero( double (*f)(const double &), const double &xl, const double &xu ){
   // El primer parámetro de esta función es un puntero a una función de la forma
   // double (*p)(const double&)
@@ -29,6 +33,7 @@ void cruces_cero( double (*f)(const double &), const double &xl, const double &x
 int main(){
   cruces_cero(funeval, 0, 100); // En este caso se llama la función Sin(M_PI*x/20)
   cruces_cero(funeval2, 0, 100);// En este otro a sin(x/10)+cos(3*x/100)
+  cruces_cero(funeval3, 20., 80);
   return 0;
 }
 
